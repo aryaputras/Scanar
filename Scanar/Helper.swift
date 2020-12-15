551e34cc431224462e159e587d866930fb7cc5e9
aryaputras <abilabil147@gmail.com>
2020-12-15 21:59:27 +0700

    first day

--- Scanar/Helper.swift
+++ Scanar/Helper.swift
@@ -5,10 +5,11 @@
 //  Created by Abigail Aryaputra Sudarman on 15/12/20.
 //
 
+import UIKit
 import Foundation
 
-var Zones = ["ACX321"]
-func GenerateUniqueRandom()->String{
+var zonesData = ["ACX321"]
+func GenerateRandom()->String{
     //Generate
     let length = 6
     let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -19,3 +20,36 @@ func GenerateUniqueRandom()->String{
      return s
     //If already exist, regenerate another
 }
+
+
+func GenerateUniqueCode() -> String{
+    var result: String
+    var unique = GenerateRandom()
+    if zonesData.contains(unique){
+        result = GenerateRandom()
+        print("generated new one")
+    } else {
+        result = unique
+        print("appended")
+    }
+    return result
+}
+
+
+
+func generateQRCode(from string: String) -> UIImage? {
+    let data = string.data(using: String.Encoding.ascii)
+
+    if let filter = CIFilter(name: "CIQRCodeGenerator") {
+        filter.setValue(data, forKey: "inputMessage")
+        let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+        if let output = filter.outputImage?.transformed(by: transform) {
+            return UIImage(ciImage: output)
+        }
+    }
+
+    return nil
+}
+
+
