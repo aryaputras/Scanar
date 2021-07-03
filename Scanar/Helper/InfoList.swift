b6b4564d94fdf7c315d6ce8c59c6307cc58d767f
aryaputras <abilabil147@gmail.com>
2021-07-03 11:47:43 +0700

    latst

---
+++ Scanar/Helper/InfoList.swift
@@ -0,0 +1,22 @@
+//
+//  InfoList.swift
+//  Scanar
+//
+//  Created by Abigail Aryaputra Sudarman on 26/02/21.
+//
+
+import Foundation
+
+struct InfoList {
+    
+    
+    
+    var useNoRef = "If no reference is selected, system will detect empty plane and place the object on top"
+    
+    var supportedFormat = "Only .obj, .dae, .usdz are supported"
+    
+    var scanWithQR = "Place the QR in front of the camera"
+    
+    var anchorGuide = "Set your object position relative to the anchor"
+}
+
--- Scanar/View Controller/AddNewZoneViewController.swift
+++ Scanar/View Controller/AddNewZoneViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import UniformTypeIdentifiers
 
-class AddNewZoneViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+class AddNewZoneViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var senderTag: Int?
     var zoneID: String?
@@ -25,7 +26,7 @@ class AddNewZoneViewController: UIViewController, UIImagePickerControllerDelegat
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         // Do any additional setup after loading the view.
         self.imagePicker = ImagePicker(presentationController: self, delegate: self)
     }
@@ -36,6 +37,17 @@ class AddNewZoneViewController: UIViewController, UIImagePickerControllerDelegat
         self.senderTag = 0
         print(sender.tag)
         self.imagePicker.present(from: sender)
+    }
+    @IBAction func importObjectClicked(_ sender: Any) {
+        let supportedTypes: [UTType] = [UTType.png,UTType.jpeg]
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: supportedTypes)
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false
+        
+        documentPicker.shouldShowFileExtensions = true
+        self.present(documentPicker, animated: true, completion: nil)
+
+        
     }
     
     @IBAction func btnClicked2(_ sender: UIButton) {
@@ -48,7 +60,7 @@ class AddNewZoneViewController: UIViewController, UIImagePickerControllerDelegat
     
     @IBAction func anchorClicked(_ sender: Any) {
     }
- 
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "objectToAnchor" {
@@ -58,7 +70,7 @@ class AddNewZoneViewController: UIViewController, UIImagePickerControllerDelegat
             vc.zoneName = zoneName
             vc.refImage = imageView.image
             vc.assetImage = imageView2.image
-
+            
             
             
             
@@ -75,20 +87,40 @@ class AddNewZoneViewController: UIViewController, UIImagePickerControllerDelegat
 extension AddNewZoneViewController: ImagePickerDelegate {
     func didSelect(image: UIImage?) {
         
-               if self.senderTag == 0 {
-                   //TROPHY 1
-                   self.imageView.image = image
-                   self.isRefSelected = true
-               } else if self.senderTag == 1 {
-                   //TROPHY 2
-                   self.imageView2.image = image
-                   self.isAssSelected = true
-               }
+        if self.senderTag == 0 {
+            //TROPHY 1
+            self.imageView.image = image
+            self.isRefSelected = true
+        } else if self.senderTag == 1 {
+            //TROPHY 2
+            self.imageView2.image = image
+            self.isAssSelected = true
+        }
     }
     
-  
- 
+    
+    
+    
+    
+    
+}
 
+
+
+
+extension AddNewZoneViewController: UIDocumentPickerDelegate {
+  
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+      
+        let url = urls.first
+       //let object = ...?
+        print(url)
+        
+        
+        controller.dismiss(animated: true)
+    }
     
-    
+    public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        controller.dismiss(animated: true)
+    }
 }
